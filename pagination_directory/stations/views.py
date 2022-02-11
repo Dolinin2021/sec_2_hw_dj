c9f8ed2d3034078446b09a8a9b5b919a22c31f19
Dolinin2021 <programmistdolinin@mail.ru>
2022-02-11 17:18:50 +0600

    Changed the views.py file in the pagination_directory folder

--- pagination_directory/stations/views.py
+++ pagination_directory/stations/views.py
@@ -4,24 +4,24 @@ from django.urls import reverse
 import csv
 
 
+stations_list = []
+
+with open('data-398-2018-08-30.csv', newline='', encoding='utf-8') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        stations_list.append(row)
+
+
 def index(request):
     return redirect(reverse('bus_stations'))
 
 
 def bus_stations(request):
-
-    stations_list = []
-
-    with open('data-398-2018-08-30.csv', newline='', encoding='utf-8') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            stations_list.append(row)
-
     paginator = Paginator(stations_list, 10)
     page_number = int(request.GET.get("page", 1))
+    page = paginator.get_page(page_number)
     context = {
-        'bus_stations': paginator.get_page(page_number),
-        'page': paginator.page(page_number),
+        'bus_stations': page.object_list,
+        'page': page,
     }
-
     return render(request, 'stations/index.html', context)
